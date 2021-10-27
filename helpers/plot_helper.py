45d3fd104e1c9a22ddd01d6f9a7f341fe8f306c6
b021 <furkan.tekin@data-boss.com.tr>
2021-10-27 15:18:44 +0300

    experiment notes and plotting

--- helpers/plot_helper.py
+++ helpers/plot_helper.py
@@ -1,5 +1,7 @@
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 
 def plot_hist_dist(in_arr, x_label="", title="", bins=25, save_path=None):
@@ -11,3 +13,45 @@ def plot_hist_dist(in_arr, x_label="", title="", bins=25, save_path=None):
     if save_path:
         plt.savefig(save_path, dpi=200)
     plt.show()
+
+
+def plot_preds():
+    plot_background(ax=ax[row_idx, col_idx], coord_range=coord_range, M=M, N=N)
+    ax[row_idx, col_idx].imshow(X=grid_sum,
+                                cmap=plt.cm.get_cmap('Reds', 10),
+                                interpolation='nearest',
+                                extent=[*coord_range[1], *coord_range[0]],
+                                alpha=0.7)
+    ax[row_idx, col_idx].set_title(crime_type, fontsize=22)
+
+
+def plot_background(ax, coord_range, M=10, N=11):
+    background_path = "background/chicago.png"
+    x_ticks = np.linspace(coord_range[1][0], coord_range[1][1], N + 1)
+    y_ticks = np.linspace(coord_range[0][0], coord_range[0][1], M + 1)
+
+    x_tick_labels = ["{:2.3f}".format(long) for long in x_ticks]
+    y_tick_labels = ["{:2.3f}".format(lat) for lat in y_ticks]
+
+    ax.set_xticks(ticks=x_ticks)
+    ax.set_xticklabels(labels=x_tick_labels,
+                  rotation=30,
+                  size=12)
+    ax.set_yticks(ticks=y_ticks)
+    ax.set_yticklabels(labels=y_tick_labels,
+                    size=12)
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+
+
+    img = mpimg.imread(background_path)
+    ax.imshow(img,
+              interpolation='bilinear',
+              extent=[*coord_range[1], *coord_range[0]])
+    ax.grid(True)
+    return ax
+
+
--- notes.md
+++ notes.md
@@ -13,3 +13,13 @@ train_val:93/94Early exiting from epoch: 100, Eval Loss: 0.43098.
 Evaluation metrics: AP:0.59740, F1:0.62434, Confusion_matrix: [TN:1401, FN:87, FP:49, TP:113]
 test:15/16Test finished, test loss: 0.42868
 Test metrics: AP:0.60869, F1:0.63443, Confusion_matrix: [TN:1408, FN:80, FP:49, TP:113]
+
+
+**graph-model (100x66)**
+Epoch: 100, Train_loss: 0.22112, Validation_loss: 0.22268, Took 761.412 seconds.
+Train metrics: AP:0.27534, F1:0.37667, Confusion_matrix: [TN:25123, FN:5427, FP:596, TP:1826]
+Validation metrics: AP:0.27431, F1:0.37719, Confusion_matrix: [TN:25067, FN:5404, FP:593, TP:1822]
+train_val:297/298Early exiting from epoch: 100, Eval Loss: 0.22226.
+Evaluation metrics: AP:0.27453, F1:0.37666, Confusion_matrix: [TN:25085, FN:5472, FP:585, TP:1836]
+test:50/51Test finished, test loss: 0.21637
+Test metrics: AP:0.27286, F1:0.37418, Confusion_matrix: [TN:25381, FN:5243, FP:620, TP:1755]
